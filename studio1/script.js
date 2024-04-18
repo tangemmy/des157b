ff4a93c02b895b8d470089be59ff4fc00ee1198e
tangemmy <emmtang@ucdavis.edu>
2024-04-17 22:05:08 -0700

    blockbuster

--- studio1/script.js
+++ studio1/script.js
@@ -3,7 +3,7 @@
     const videos = document.querySelectorAll("video");
     const bgm = new Audio("sound/chill.mp3");
     bgm.loop = true;
-    
+
     window.addEventListener("mousemove", function() {
         bgm.play();
     });
@@ -30,5 +30,30 @@
         });
     });
 
+//text
+    const myVideo = document.querySelector("#v5");
+    const line1 = document.querySelector('#line1');
+    const line2 = document.querySelector('#line2');
+    const line3 = document.querySelector('#line3');
+    const line4 = document.querySelector('#line4');
+    const line5 = document.querySelector('#line5');
+    const quote = {
+    start: [0, 5, 8, 11,16],
+    stop: [4, 7, 10, 15,18],
+    line: [line1, line2, line3,line4,line5]
+    }
 
+    const intervalID = setInterval(checkTime, 1000); //every second they check time
+
+    function checkTime() {
+    console.log(parseInt(myVideo.currentTime));
+
+    for (let i = 0; i < quote.start.length; i++) {
+        if (quote.start[i] < myVideo.currentTime && myVideo.currentTime < quote.stop[i]) {
+        quote.line[i].className = "showing";
+        } else {
+        quote.line[i].className = "hidden";
+        }
+    }
+    }
 })();
