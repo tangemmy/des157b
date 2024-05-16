78249e86c161f9c870892de252eeaf6264c102d2
tangemmy <emmtang@ucdavis.edu>
2024-05-15 22:47:53 -0700

    libary

--- ministudio/studio3/script.js
+++ ministudio/studio3/script.js
@@ -1,4 +1,19 @@
-(function(){
+
     // initialize AOS here
-    AOS.init();
-    })();
+  AOS.init();  
+
+    let capture;
+    function setup(){
+        createCanvas(320,240);
+        capture=createCapture(VIDEO);
+        capture.size(320,240);
+        capture.hide();
+    }
+    function draw(){
+        background(255);
+        image(capture, 0, 0, 320, 240);
+        filter('erode');
+    }
+   // Global variable to hold the video capture objects
+
+
